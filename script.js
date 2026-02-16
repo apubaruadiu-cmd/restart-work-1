c3b5b768a7bf5e2b119298ddfa5e171e2513a51e
apubaruadiu-cmd <apubarua.diu@gmail.com>
2026-02-16 23:07:26 +0600

    Modal Design & dynamic

--- script.js
+++ script.js
@@ -22,6 +22,34 @@ const loadAllProducts = () => {
     });
 };
 
+const loadProductDetail = async(id) => {
+    const url = `https://fakestoreapi.com/products/${id}`;
+    const res = await fetch(url);
+    const details = await res.json();
+    displayProductDetails(details);
+}
+
+const displayProductDetails = (details) => {
+    const detailsBox = document.getElementById("details-container");
+    detailsBox.innerHTML = `
+        <div>
+            <div class="text-2xl font-bold mb-2">
+              ${details.title}
+            </div>
+            <div class="text-gray-500">
+              <i class="fa-solid fa-star text-warning"></i> ${details.rating.rate} (${details.rating.count})
+            </div>
+          </div>
+          <div class="text-md font-medium text-gray-600">
+            ${details.description}
+          </div>
+          <div class="text-3xl font-bold">$${details.price}</div>
+          <button class="btn btn-primary">Buy Now</button>
+    `;
+
+    document.getElementById("my_modal").showModal();
+}
+
 const displayAllProducts = (allproducts) => {
     const productContainer = document.getElementById("products-container");
     productContainer.innerHTML = "";
@@ -36,13 +64,13 @@ const displayAllProducts = (allproducts) => {
                         </figure>
                         <div class="card-body">
                             <div class="badge-rating flex justify-between items-center mb-4">
-                                <div class="badge badge-soft badge-primary">Mens Clothing</div>
-                                <div class="text-gray-500"><i class="fa-solid fa-star text-warning"></i> 3.9 (120)</div>
+                                <div class="badge badge-soft badge-primary">${product.category}</div>
+                                <div class="text-gray-500"><i class="fa-solid fa-star text-warning"></i> ${product.rating.rate} (${product.rating.count})</div>
                             </div>
                             <h2 class="card-title truncate font-medium text-2xl block">${product.title}</h2>
                             <div class="product-price font-bold text-xl">$${product.price}</div>
                             <div class="card-actions mt-6">
-                                <button class="btn btn-outline flex-1"><i class="fa-regular fa-eye"></i> Details</button>
+                                <button onclick="loadProductDetail(${product.id})" class="btn btn-outline flex-1"><i class="fa-regular fa-eye"></i> Details</button>
                                 <button class="btn btn-primary flex-1"><i class="fa-solid fa-cart-shopping"></i> Add</button>
                             </div>
                         </div>
@@ -80,13 +108,13 @@ const displayProductCategory = (products) => {
                         </figure>
                         <div class="card-body">
                             <div class="badge-rating flex justify-between items-center mb-4">
-                                <div class="badge badge-soft badge-primary">Mens Clothing</div>
-                                <div class="text-gray-500"><i class="fa-solid fa-star text-warning"></i> 3.9 (120)</div>
+                                <div class="badge badge-soft badge-primary">${product.category}</div>
+                                <div class="text-gray-500"><i class="fa-solid fa-star text-warning"></i> ${product.rating.rate} (${product.rating.count})</div>
                             </div>
                             <h2 class="card-title truncate font-medium text-2xl block">${product.title}</h2>
                             <div class="product-price font-bold text-xl">$${product.price}</div>
                             <div class="card-actions mt-6">
-                                <button class="btn btn-outline flex-1"><i class="fa-regular fa-eye"></i> Details</button>
+                                <button onclick="loadProductDetail(${product.id})" class="btn btn-outline flex-1"><i class="fa-regular fa-eye"></i> Details</button>
                                 <button class="btn btn-primary flex-1"><i class="fa-solid fa-cart-shopping"></i> Add</button>
                             </div>
                         </div>
